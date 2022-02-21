fdb61277fd3cb001c6245b915ecc31d8363454d0
Andre <quadhay@gmail.com>
2022-02-21 11:10:02 +0100

    Saved

--- src/layout/NotificationLayout/index.js
+++ src/layout/NotificationLayout/index.js
@@ -1,4 +1,5 @@
 import {AiOutlineRise} from 'react-icons/ai'
+import {AiOutlineFall} from 'react-icons/ai'
 import {Button} from 'react-bootstrap'
 
 const Index = ()=>{
@@ -7,9 +8,22 @@ const Index = ()=>{
             <div className="transaction">
                 <div className="notifyDiv">
                     <div className='notify-flex-1'>
-                        <div>Withdrawal Alert</div>
-                        <div className="notifyAmount">BTC 0.0005 <span><AiOutlineRise color='green' size={30}/></span> </div>
+                        <div style={{color:'red'}}>Withdrawal Alert</div>
+                        <div className="notifyAmount">BTC 0.0005 <span><AiOutlineRise color='red' size={30}/></span> </div>
                         <small>You have Initiated Transfer of 0.0005 BTC to yuyuinbjgnbngnnbbnbnbnbnb Address</small>
+                    </div>
+                    <div className='notify-flex-2'>
+                       <Button>View Details</Button>
+                    </div>
+                     
+                </div>
+
+
+                <div className="notifyDiv">
+                    <div className='notify-flex-1'>
+                        <div style={{color:'green'}}>Deposit Alert</div>
+                        <div className="notifyAmount">BTC 0.0005 <span><AiOutlineFall color='green' size={30} /></span> </div>
+                        <small>You have received a  Transfer of 0.0005 BTC from yuyuinbjgnbngnnbbnbnbnbnb Address</small>
                     </div>
                     <div className='notify-flex-2'>
                        <Button>View Details</Button>
