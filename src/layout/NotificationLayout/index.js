35cb4e22a9a67941621d91f39ddf4b1a0c15ef3c
Andre <quadhay@gmail.com>
2022-02-21 11:02:11 +0100

    Saved

--- src/layout/NotificationLayout/index.js
+++ src/layout/NotificationLayout/index.js
@@ -1,8 +1,24 @@
+import {AiOutlineRise} from 'react-icons/ai'
+import {Button} from 'react-bootstrap'
+
 const Index = ()=>{
     return (
-        <div>
-            <h1>Welcome to Notification</h1>
+        
+            <div className="transaction">
+                <div className="notifyDiv">
+                    <div className='notify-flex-1'>
+                        <div>Withdrawal Alert</div>
+                        <div className="notifyAmount">BTC 0.0005 <span><AiOutlineRise color='green' size={30}/></span> </div>
+                        <small>You have Initiated Transfer of 0.0005 BTC to yuyuinbjgnbngnnbbnbnbnbnb Address</small>
+                    </div>
+                    <div className='notify-flex-2'>
+                       <Button>View Details</Button>
+                    </div>
+                     
+                </div>
+                
         </div>
+        
     )
 }
 
