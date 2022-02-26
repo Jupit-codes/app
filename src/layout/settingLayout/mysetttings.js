e707f21c6f340e99982eac4bd84a94f12f6ccfbf
Andre <quadhay@gmail.com>
2022-02-26 15:06:11 +0100

    Saved

--- src/layout/settingLayout/mysetttings.js
+++ src/layout/settingLayout/mysetttings.js
@@ -1,29 +1,47 @@
 import '../../assets/css/settings/profile.css'
 import {FaEnvelope} from 'react-icons/fa'
 import {BsTelephone} from 'react-icons/bs'
+import { reactLocalStorage } from 'reactjs-localstorage'
+import { useEffect, useState } from 'react'
+import { Tab } from './tab'
 const Index=()=>{
+    const [Kyc,setKyc] = useState();
+    useEffect(()=>{
+       var kyc_level1 =  reactLocalStorage.getObject('kyc').level1[0].status;
+       var kyc_level2 =  reactLocalStorage.getObject('kyc').level2[0].event_status;
+       var kyc_level3 =  "Undefined"
+       if(kyc_level1 === "Verified"){
+           setKyc('Level 1')
+       }
+       else if(kyc_level1 === "Verified" && kyc_level2 === "Verified" ){
+           setKyc('Level 2')
+       }
+       else if(kyc_level1 === "Verified" && kyc_level2 === "Verified" && kyc_level3 === "Verified"){
+           setKyc('Level 3')
+       }
+    },[])
     return (
         <div className="settings-profile">
             <div className="profile">
-                Profile
+               <Tab/>
             </div>
             <div className="myprofile">
                 
                 <div className="profile-pic">
 
                 </div>
-                <div className="profile-name">Temiloluwa Odewumi</div>
-                <div className="email"> <FaEnvelope size={15} style={{marginRight:10}}/> bigdevtemy@gmail.com</div>
-                <div className="phonenumber"> <BsTelephone size={15} style={{marginRight:10}}/> 07060934005</div>
-                <div className="kyc-level"> KYC LEVEL :0</div>
+                <div className="profile-name">{reactLocalStorage.getObject('user').username}</div>
+                <div className="email"> <FaEnvelope size={15} style={{marginRight:10}}/>{reactLocalStorage.getObject('user').email}</div>
+                <div className="phonenumber"> <BsTelephone size={15} style={{marginRight:10}}/> {reactLocalStorage.getObject('user').phonenumber}</div>
+                <div className="kyc-level"> KYC LEVEL :{Kyc}</div>
 
                 <div className="acct"> PRIMARY BANK ACCOUNT :
-                    <span>Access Bank PLC </span>
-                    <span>0690086425 </span>
+                    <span>Bank:NULL </span>
+                    <span>Account Number:NULL </span>
                 </div>
                 <div className="acct"> ALTERNATE BANK ACCOUNT :
-                    <span>Access Bank PLC </span>
-                    <span>0690086425 </span>
+                    <span>NULL </span>
+                    {/* <span>NULL </span> */}
                 </div>
             </div>
         </div>
---
+++ src/layout/settingLayout/tab.js
@@ -0,0 +1,16 @@
+import '../../assets/css/settings/tab.css'
+export const Tab = ()=>{
+    return(
+        <div className="tabSettings">
+            <div className='tabDiv'>
+                Account Settings
+            </div>
+            <div className='tabDiv'>
+                Notification Settings
+            </div>
+            <div className='tabDiv'>
+                Security Settings
+            </div>
+        </div>
+    )
+}
