4cffa06b61768d7d5ddf0d4121084b6d429ec239
Andre <quadhay@gmail.com>
2022-03-03 15:26:45 +0100

    saved

--- src/layout/HomeLayout/GetIntouch/index.js
+++ src/layout/HomeLayout/GetIntouch/index.js
@@ -2,7 +2,7 @@ import '../../../assets/css/GetInTouch/getintouch.css'
 const Index = ()=>{
     return(
         <div className="getIntouch">
-            <h1>Akute</h1>
+            <h1>Get In Touch</h1>
         </div>
     )
 }
