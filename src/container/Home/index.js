b422f7322cba54f36212a01078dcf597acc1afba
Andre <quadhay@gmail.com>
2022-03-03 11:41:25 +0100

    Saved

--- src/container/Home/index.js
+++ src/container/Home/index.js
@@ -30,6 +30,7 @@ const Index=()=>{
             <HowItWorks/>
             <OurOffer/>
             <Process/>
+            <Section4/>
             
             {/* <Whoweare/> */}
             {/* <Hero/>
--- src/layout/HomeLayout/ProcessFlow/index.js
+++ src/layout/HomeLayout/ProcessFlow/index.js
@@ -4,7 +4,32 @@ const Index = ()=>{
         <div className="process">
                  <div className="process-parallax"></div>
                  <div className='stepDiv'>
-                    Hello
+                    <h1>How to Get Started</h1>
+                 
+                    <div className='underline'><hr/></div>
+                    <div className='buildStep'>
+                        <div className='step'>
+                            <div className='count'>01</div>
+                            <div className='countDetails'>
+                                <div className='countDetailsTitle'>SignUp For Jupit</div>
+                                <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly.</p>
+                            </div>
+                        </div>
+                        <div className='step'>
+                        <div className='count'>02</div>
+                            <div className='countDetails'>
+                                <div className='countDetailsTitle'>Connect To Bank</div>
+                                <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly.</p>
+                            </div>
+                        </div>
+                        <div className='step'>
+                        <div className='count'>03</div>
+                            <div className='countDetails'>
+                                <div className='countDetailsTitle'>Start Trading</div>
+                                <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly.</p>
+                            </div>
+                        </div>
+                    </div>
                  </div>
         </div>
     )
