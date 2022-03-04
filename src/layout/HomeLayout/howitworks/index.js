b12f38a5c0c8786c32c75e4329f48b6bcac7e408
Andre <quadhay@gmail.com>
2022-03-04 11:43:50 +0100

    Saved

--- src/layout/HomeLayout/howitworks/index.js
+++ src/layout/HomeLayout/howitworks/index.js
@@ -1,13 +1,20 @@
 import srcImg from '../../../assets/images/section-bg/section-bg2.jpg'
 import '../../../assets/css/howitworks/howitworks.css'
 import {FaPlay} from 'react-icons/fa'
+import VideoModal from '../../../utils/modal/video.js'
+import { useState } from 'react'
 const Index = ()=>{
+	const [modal, setmodal] = useState(false)
+	const play =()=>{
+		setmodal(true)
+	}
     return (
         <div class="sectionDiv">
+			{modal && <VideoModal closeModal={setmodal}/>}
 				<div className='parallax'></div>
 				<div className='punditX'>
-					<div>
-						<FaPlay  size={50}/>
+					<div onClick={()=>play()} className="play">
+						<FaPlay  size={50} />
 					</div>
 					<div className='howitworksTitle'>
 						How Its Works?
---
+++ src/utils/modal/video.js
@@ -0,0 +1,43 @@
+import {useState,useEffect} from 'react'
+import React from 'react'
+import '../../assets/css/Modal/modal.css'
+import {IoClose} from 'react-icons/io5'
+import ReactPlayer from 'react-player'
+import Video from '../../assets/images/video.mp4'
+const Index = ({closeModal})=>{
+ 
+  
+    return (
+        <div className="modalBackground">
+            <div className='modalContainerII videobody'>
+                <div className='modalHeader'>
+                    <div className='modalText'>
+                        <div className='receiveText'>
+                            Jupit Pundit
+                        </div>
+                        
+                            <IoClose size={25} color="#fff" onClick={()=>closeModal(!closeModal)} />
+                    </div>
+                    
+                    <div><hr/></div>
+                </div>
+                
+                <div className='camerabody videobody'>
+                 <ReactPlayer 
+                 playing
+                 controls
+                 url={Video} />
+                   
+                </div>
+                <div className='modalFooter'>
+                   
+                </div>
+                {/* <div className='modalClose' >
+                    <input type="submit" value="Close" onClick={()=>closeModal(!closeModal)} />
+                </div> */}
+            </div>
+        </div>
+    )
+}
+
+export default Index;
