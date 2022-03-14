c8b0c3a4192ccfd0de749bcc1a1d4d637104c8b5
wabloan <wabloan@wabloan.com>
2022-03-14 10:12:51 +0100

    saved

--- src/layout/HomeLayout/Hero3/index.js
+++ src/layout/HomeLayout/Hero3/index.js
@@ -21,7 +21,7 @@ const Index = ()=>{
     // },[animateImg])
 
     const Herotext = ()=>{
-        return  <span style={{color:'#00C0CE'}}>
+        return  <span style={{color:'#1c1c93'}}>
                         <Typewriter
                             options={{
                             // strings: ['Hello', 'World'],
