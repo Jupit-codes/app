72856542edec6edd39fd3ba046e1ad9bdb24923a
wabloan <wabloan@wabloan.com>
2022-03-29 13:30:42 +0100

    saved

--- src/component/xtitlebar/index.js
+++ src/component/xtitlebar/index.js
@@ -1,5 +1,8 @@
 import '../../assets/css/xtitlebar/xtitlebar.css'
-
+import {IoIosNotificationsOutline} from 'react-icons/io'
+import {BiSupport} from 'react-icons/bi'
+import {IoIosChatbubbles} from 'react-icons/io'
+import ProfileImage from '../../assets/images/utility/profile-pic.png'
 const Index = ()=>{
     return (
         <div className="xtitle">
@@ -16,12 +19,27 @@ const Index = ()=>{
                
 
                 <div className='profilesection'>
-                    <div>
-                        logout
+                    <div className='xcode'>  
+
+                        <div className='Notification'>
+                            <IoIosNotificationsOutline size={20} color="#1c1c93"/>
+                        </div>
+                        <div className='Notification'>
+                            <BiSupport size={20} color="#1c1c93"/>
+                        </div>
+                        <div className='Notification'>
+                            <IoIosChatbubbles size={20} color="#1c1c93"/>
+                        </div>
+                        {/* <div className='ProfileName'>
+                            Geoffrey
+                        </div> */}
+                        
+                        <div className='profileImage'>
+                           <img src={ProfileImage}/>
+                        </div>
+
                     </div>
-                    <div>
-                        12:22pm, 29 March 2022.
-                    </div>
+                    
 
                 </div>
         </div>
