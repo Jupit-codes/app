8b7e25546e0448761d2cba2b980da4070d46e308
wabloan <wabloan@wabloan.com>
2022-04-01 11:14:04 +0100

    saved

--- src/layout/BodyLayout/Section2/index.js
+++ src/layout/BodyLayout/Section2/index.js
@@ -1,7 +1,7 @@
 import '../../../assets/css/Body/section2.css'
 import { Chart, LineAdvance} from 'bizcharts';
 import LineChart from '../lineChart.js'
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Naira from '../../../assets/images/utility/naira.png'
 import Illustrator from '../../../assets/images/utility/Illustration.png'
 import Ornament from '../../../assets/images/utility/Ornament.png'
@@ -9,7 +9,11 @@ import jupit from '../../../assets/images/utility/jupit.png'
 import cardType from '../../../assets/images/utility/mastercard.png'
 import { DropdownButton,Dropdown } from 'react-bootstrap';
 const Index = ()=>{
-  
+    const [showButton,setshowButton] = useState(false)
+    const handleBalanceChangeBalance = ()=>{
+        setshowButton(!showButton)
+        alert('clicked')
+    }
     return(
         <div className="body_section2">
 
@@ -39,14 +43,14 @@ const Index = ()=>{
                             Balances
                         </div>
                         <div className='balanceIcon'>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#1c1c93" class="bi bi-three-dots-vertical" viewBox="0 0 16 16">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#1c1c93" class="bi bi-three-dots-vertical" viewBox="0 0 16 16" onClick={()=>{setshowButton(!showButton)}}>
                             <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
                         </svg>
                         
                         </div>
                         
                     </div>
-                    <div className='type'>
+                    <div className={showButton ? 'type showtype':'type'} >
                         <div>BTC</div>
                         <div>USDT</div>
                         <div>NAIRA</div>
