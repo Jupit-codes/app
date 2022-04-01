e9b070152ea05cab85ea026f095c258ad29af77c
wabloan <wabloan@wabloan.com>
2022-04-01 12:13:11 +0100

    saved

---
+++ src/layout/BodyLayout/Section2/USDT.js
@@ -0,0 +1,59 @@
+import jupit from '../../../assets/images/utility/jupit.png'
+import cardType from '../../../assets/images/utility/mastercard.png'
+const Index = ()=>{
+    return (
+        <div className='designcover slideCardLeft'>
+                            <div className='cardNairaCover'>
+                                <div className='overlay'>
+
+                                </div>
+                                <div className='cardNaira'>
+                                    <div className='card_section_a'>
+                                        <div>
+                                            <img src={jupit}/>
+                                        </div>
+                                        <div>
+                                            **** **** **** 0706
+                                        </div>
+
+                                    </div>
+                                    <div className='card_section_b'>
+                                        <div className='card_section_main'>
+                                            Usdt Balance
+
+                                        </div>
+                                        <div className='card_section_balance'>
+                                            $12,000,000
+                                        </div>
+                                    </div>
+                                    <div className='card_section_c'>
+                                        <div>
+                                            VALID THRU<br/>
+                                            03/09
+                                        </div>
+                                        <div>
+                                            CARD HOLDER<br/>
+                                            ODEWUMI TEMILOLUWA
+                                        </div>
+                                        <div>
+                                            <img src={cardType} />
+                                        </div>
+                                    </div>
+
+
+                                </div>
+                            
+                            </div>
+                            <div className='cardDesign'>
+                                <div className='cardBTC'>
+                                        
+                                </div>
+                                <div className='cardUSDT'>
+                                        
+                                </div>
+                            </div>
+                        </div>
+    )
+}
+
+export default Index
--- src/layout/BodyLayout/Section2/index.js
+++ src/layout/BodyLayout/Section2/index.js
@@ -5,17 +5,56 @@ import { useEffect, useState } from 'react';
 import Naira from '../../../assets/images/utility/naira.png'
 import Illustrator from '../../../assets/images/utility/Illustration.png'
 import Ornament from '../../../assets/images/utility/Ornament.png'
-import jupit from '../../../assets/images/utility/jupit.png'
-import cardType from '../../../assets/images/utility/mastercard.png'
+
 import { DropdownButton,Dropdown } from 'react-bootstrap';
+import Btc from './BTC.js'
+import NairaWallet from './NAIRA.js'
+import Usdt from './USDT.js'
 const Index = ()=>{
     const [showButton,setshowButton] = useState(false)
-    const handleBalanceChangeBalance = ()=>{
-        setshowButton(!showButton)
-        alert('clicked')
+    const [component,setComponent] = useState('Naira')
+    const _renderComponent = ()=>{
+        switch (component) {
+            case 'Naira':
+                return <NairaWallet comp={setComponent}/>
+                break;
+            case 'Btc':
+                return <Btc comp={setComponent}/>
+                break;
+            case 'Usdt':
+                return <Usdt comp={setComponent}/>
+                break;
+        
+            default:
+                break;
+        }
     }
     return(
         <div className="body_section2">
+            <div className='walletbalance'>
+                    <div className='walletBalanceTitle'>
+                        <div className='balanceWallet'>
+                            Balances
+                        </div>
+                        <div className='balanceIcon'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#1c1c93" class="bi bi-three-dots-vertical" viewBox="0 0 16 16" onClick={()=>{setshowButton(!showButton)}}>
+                            <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
+                        </svg>
+                        
+                        </div>
+                        
+                    </div>
+                    <div className={showButton ? 'type showtype':'type'} >
+                        <div onClick={()=>{setComponent('Btc');setshowButton(!showButton)}}>BTC</div>
+                        <div onClick={()=>{setComponent('Usdt');setshowButton(!showButton)}}>USDT</div>
+                        <div onClick={()=>{setComponent('Naira');setshowButton(!showButton)}}>NAIRA</div>
+                    </div>
+                    <div className='rendercomponent'>
+                        {_renderComponent()}
+                    </div>
+                    
+
+            </div>
 
             <div className='marketTransaction'>
                 <div className='marketTitle'>
@@ -37,80 +76,7 @@ const Index = ()=>{
                 </div>
                <LineChart/>
             </div>
-            <div className='walletbalance'>
-                    <div className='walletBalanceTitle'>
-                        <div className='balanceWallet'>
-                            Balances
-                        </div>
-                        <div className='balanceIcon'>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#1c1c93" class="bi bi-three-dots-vertical" viewBox="0 0 16 16" onClick={()=>{setshowButton(!showButton)}}>
-                            <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
-                        </svg>
-                        
-                        </div>
-                        
-                    </div>
-                    <div className={showButton ? 'type showtype':'type'} >
-                        <div>BTC</div>
-                        <div>USDT</div>
-                        <div>NAIRA</div>
-                    </div>
-                    
-
-                    
-   
-                    <div className='designcover'>
-                    <div className='cardNairaCover'>
-                        <div className='overlay'>
-
-                        </div>
-                        <div className='cardNaira'>
-                            <div className='card_section_a'>
-                                <div>
-                                    <img src={jupit}/>
-                                </div>
-                                <div>
-                                    **** **** **** 1234
-                                </div>
-
-                            </div>
-                            <div className='card_section_b'>
-                                <div className='card_section_main'>
-                                    Main Balance
-
-                                </div>
-                                <div className='card_section_balance'>
-                                    $12,000,000
-                                </div>
-                            </div>
-                            <div className='card_section_c'>
-                                <div>
-                                    VALID THRU<br/>
-                                    03/09
-                                </div>
-                                <div>
-                                    CARD HOLDER<br/>
-                                    ODEWUMI TEMILOLUWA
-                                </div>
-                                <div>
-                                    <img src={cardType} />
-                                </div>
-                            </div>
-
-
-                        </div>
-                       
-                    </div>
-                        <div className='cardDesign'>
-                            <div className='cardBTC'>
-                                    
-                            </div>
-                            <div className='cardUSDT'>
-                                    
-                            </div>
-                        </div>
-                    </div>
-            </div>
+            
         </div>
     )
 }
