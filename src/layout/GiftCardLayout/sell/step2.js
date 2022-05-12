8d40502ab131250af56f9b4ae3c494affe08befc
wabloan <wabloan@wabloan.com>
2022-05-12 23:30:46 +0100

    Saved

--- src/layout/GiftCardLayout/sell/step2.js
+++ src/layout/GiftCardLayout/sell/step2.js
@@ -1,6 +1,7 @@
 import { getStepButtonUtilityClass } from '@mui/material';
 import { useEffect, useState } from 'react';
-import {BsArrowLeftCircle} from 'react-icons/bs'
+import {BsArrowLeftCircle,BsCartPlusFill} from 'react-icons/bs'
+
 const Index = ({stepPage,checked})=>{
 
     const [quantity,setquantity] = useState(0)
@@ -38,33 +39,39 @@ useEffect(()=>{
                 <div className='rateDiv myformSell'>
                     <div className='Selectrate'>
                           
-                           <div className='form-floating'>
-                               <select className='form-control floatingselectAmount' id="floatingselectAmount" onChange={handleSelect}>
-                                    <option>
-                                       Click
-                                    </option>
-                                   <option value={10}>
-                                       10USD
-                                   </option>
-                                   <option value={150}>
-                                       150USD
-                                   </option>
-                                   <option value={200}>
-                                       200USD
-                                   </option>
-                               </select>
-                               <label for="floatingselectAmount">Select Amount</label>
+                           <div className='calculator'>
+                                <div className='form-floating'>
+                                    <select className='form-control floatingselectAmount' id="floatingselectAmount" onChange={handleSelect}>
+                                            <option>
+                                            Click
+                                            </option>
+                                        <option value={250}>
+                                            10USD &#8781; &#8358;250
+                                        </option>
+                                        <option value={300}>
+                                            150USD &#8781; &#8358;300
+                                        </option>
+                                        <option value={400}>
+                                            200USD &#8781; &#8358;400
+                                        </option>
+                                    </select>
+                                    <label for="floatingselectAmount">Select Amount</label>
+                                </div>
+
+                                <div className='form-floating'>
+                                        <input type="number" id="floatingnumber" className="form-control floatingnumber" onChange={handleQuantity} value={quantity} min={0}/>
+                                        <label for="floatingnumber">Quantity</label>
+                                </div>
+
+                                <div className='form-floating sum'>
+                                        
+                                        <input type="number" id="floatingsum" className="form-control floatingnumber"  value={total} disabled={true}/>
+                                        <label for="floatingsum">Sum Total</label>
+                                </div>
                            </div>
 
-                           <div className='form-floating'>
-                                <input type="number" id="floatingnumber" className="form-control floatingnumber" onChange={handleQuantity} value={quantity} min={0}/>
-                                <label for="floatingnumber">Quantity</label>
-                           </div>
-
-                           <div className='form-floating sum'>
-                                
-                                <input type="number" id="floatingsum" className="form-control floatingnumber"  value={total} disabled={true}/>
-                                <label for="floatingsum">Sum Total</label>
+                           <div className='cartplus'>
+                              <BsCartPlusFill size={30} color="#259c77"/>
                            </div>
                     </div>
                     <div className='cartSummary'>
