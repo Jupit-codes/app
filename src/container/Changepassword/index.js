5b086fb6fcd51b630b8e9c9bd3aa366a780afeac
wabloan <wabloan@wabloan.com>
2022-05-16 10:16:27 +0100

    saved

--- src/container/Changepassword/index.js
+++ src/container/Changepassword/index.js
@@ -2,8 +2,42 @@ import Header from '../../component/changeHeader'
 import '../../assets/css/changepassword/change.css'
 import Cookies from 'js-cookie';
 import {AiFillUnlock} from 'react-icons/ai'
+import { useEffect, useState } from 'react';
+import { reactLocalStorage } from 'reactjs-localstorage';
+import axios from 'axios';
+
 const Index = ()=>{
     console.log(Cookies.get('item'))
+    const [myloader,setmyloader] = useState(false)
+    const Base_url = process.env.REACT_APP_BACKEND_URL
+    const getSession=async ()=>{
+        await axios({
+            method: "GET",
+            url: `${Base_url}/user/getSession/data`,
+            headers:{
+                'Content-Type':'application/json',
+                'Authorization':reactLocalStorage.get('token')
+            },
+           // data:JSON.stringify({userid:reactLocalStorage.getObject('user')._id})
+        })
+        .then((res)=>{
+            
+            setmyloader(false)
+            console.log(res.data)
+            
+          
+        })
+        .catch((err)=>{
+            setmyloader(false)
+            console.log(err.response)
+            //setErr(err.response? err.response.data:'NO Connecetion')
+            
+        })
+    }
+
+    useEffect(()=>{
+        getSession();
+    },[])
     return(
         <div className='changepassword'>
             <Header changepassword={5}/>
