d34a5eb60befe1945c601007a7868086b03f0fca
wabloan <wabloan@wabloan.com>
2022-06-29 16:29:19 +0100

    ok

--- src/container/Forgetpassword/index.js
+++ src/container/Forgetpassword/index.js
@@ -12,10 +12,11 @@ import 'react-toastify/dist/ReactToastify.css';
 import {reactLocalStorage} from 'reactjs-localstorage';
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import axios from 'axios'
-
+import {BiArrowBack} from 'react-icons/bi'
 
 const Index=()=>{
     // const {authDispatch,authState:{auth:{loading,data,error,errorAlert}}} = useContext(GlobalContext)
+    const history = useHistory();
     const [section,setsection] = useState('Section1');
     const [emailSection,setEmailSection] = useState();
     const [passwordSection,setPasswordSection] = useState();
@@ -28,6 +29,7 @@ const Index=()=>{
     const [error,seterror] = useState()
     const [data,setdata] = useState()
 
+
     const handleEmail = (e)=>{
         setemail(e.target.value)
     }
@@ -38,7 +40,7 @@ const Index=()=>{
         seterror('')
         setdata('')
 
-        
+
     axios({
         method: "POST",
         url: `https://myjupit.herokuapp.com/users/resetpassword`,
@@ -62,7 +64,9 @@ const Index=()=>{
     })
 }
  
- 
+    const back = ()=>{
+        history.push('/')
+    }
   
 
 
@@ -73,11 +77,14 @@ const Index=()=>{
                         <div className="authCardReset">
                             <div className="mylogo_new"><Link to='/'><img src={Jupit}  className="mylogo_me"/></Link></div>
                             <div className="welcomeback"> PASSSWORD RESET</div>
-                           
+                           <div className="formClass">
+                                    <BiArrowBack  size={20} onClick={()=>back()} style={{cursor:'pointer'}}/>&nbsp;Back
+                           </div>
                             <div className="customerror">{error && <AlertDismissible itemData={error} itemState="error"/>}</div>
                             <div className="customerror">{data && <AlertDismissible itemData={data} itemState="success"/>}</div>
                             <div className="formArea">
                                 <div className="formClass">
+                                
                                     <ToastContainer/>
                                     {loading && <Loader />}
                                     <form className="form-input">
