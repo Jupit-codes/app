face67a3e52a454fa7ab20b9a518c934df350f1b
wabloan <wabloan@wabloan.com>
2022-06-29 13:12:30 +0100

    ok

--- src/container/Register/index.js
+++ src/container/Register/index.js
@@ -12,8 +12,20 @@ import AlertDismissible from '../../utils/alert/alertDisplay'
 import AlertSuccess from '../../utils/alert/success'
 const Index=()=>{
     const {registerDispatch,registerState:{registerAuth:{loading,data,error,errorAlert}}} = useContext(GlobalContext);
-    console.log('RegisterError',error)
     
+    useEffect(() => {
+        try {
+          // trying to use new API - https://developer.mozilla.org/en-US/docs/Web/API/Window/scrollTo
+          window.scroll({
+            top: 0,
+            left: 0,
+            behavior: 'smooth',
+          });
+        } catch (error) {
+          // just a fallback for older browsers
+          window.scrollTo(0, 0);
+        }
+      }, [loading]);
     return(
         <div className="auth">
                 {loading && <Loader/>}
