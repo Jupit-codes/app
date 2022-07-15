613ce160d8a9bfe14c0e63fdc28b2f2d17321e4d
wabloan <wabloan@wabloan.com>
2022-07-16 00:02:34 +0100

    ok

--- src/container/Login/section1/index.js
+++ src/container/Login/section1/index.js
@@ -87,7 +87,7 @@ const Index=({Next,setUserPassword,setUserEmail})=>{
                             <input type="submit" className="form-control mybtn" onClick={submitLogin} value="Login"/>
                         </div>
                         <div className="additional">
-                            <span className="new-customer">New Customer? </span><Link style={{textDecoration:'none'}} to='/client/signup'><span className="create_account">Create an Account</span></Link>
+                            {/* <span className="new-customer">New Customer? </span><Link style={{textDecoration:'none'}} to='/client/signup'><span className="create_account">Create an Account</span></Link> */}
                         </div>
                         
                     </form>
