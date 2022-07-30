b32696de0c92a8a3d2a2398dd1137c0fbffd3202
wabloan <wabloan@wabloan.com>
2022-07-30 07:42:04 +0100

    okay

--- src/container/Dashboard/index.js
+++ src/container/Dashboard/index.js
@@ -28,7 +28,7 @@ import {BsWallet2} from 'react-icons/bs'
 import {AiOutlineWhatsApp} from 'react-icons/ai'
 import {BiLogOut} from 'react-icons/bi'
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
-import jupit_logo_white from '../../assets/images/jupit_logo_white.svg'
+import jupit_white from '../../assets/images/jupit_white.svg'
 
 const Index=()=>{
     const location = useLocation();
@@ -194,7 +194,8 @@ const Index=()=>{
                              </div>
 
                              <div className="jupit-side-bar-logo">
-                                <img src={jupit_logo_white}/>
+                                <img src={jupit_white}/>
+                                <div>..fast and secure</div>
                              </div>
                             
                             
