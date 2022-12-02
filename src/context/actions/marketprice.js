95d679c8cc1fd071d3bfa49b8eebe700f636bc29
TEMILOLUWA <bigdevtemy@gmail.com>
2022-12-02 13:57:47 +0100

    pressed

--- src/context/actions/marketprice.js
+++ src/context/actions/marketprice.js
@@ -5,17 +5,20 @@ export default ()=>(dispatch)=>{
     dispatch({
         type:PRICE_LOADING
     })
-axios.get('https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,USDT&tsyms=USD',{
+
+    //https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,USDT&tsyms=USD
+    //Apikey fab6779bb25e937fa7ef922e132796d2c323635c431bc1f3185faf7b293633c5
+axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=tether,bitcoin&order=market_cap_desc&per_page=100&page=1&sparkline=false',{
     headers:{
         'Content-Type':'application/json',
-        'Authorization':'Apikey fab6779bb25e937fa7ef922e132796d2c323635c431bc1f3185faf7b293633c5'
+        // 'Authorization':'Apikey fab6779bb25e937fa7ef922e132796d2c323635c431bc1f3185faf7b293633c5'
     }
 })
 .then(res=>{
-    // console.log(res.data)
+    console.log(res.data)
     dispatch({
         type:PRICE_SUCCESS,
-        payload:res.data.RAW
+        payload:res.data
     })
 })
 .catch(err=>{
