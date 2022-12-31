619804359c4b2018f4430bcf73344b535ce7ccc8
TEMILOLUWA <bigdevtemy@gmail.com>
2022-12-31 19:34:15 +0100

    kopied

--- src/utils/modal/customModal.js
+++ src/utils/modal/customModal.js
@@ -5,17 +5,51 @@ import {IoClose} from 'react-icons/io5'
 import QRcode from 'qrcode'
 import { reactLocalStorage } from 'reactjs-localstorage'
 import {CopyToClipboard} from 'react-copy-to-clipboard';
+import axios from 'axios'
 const Index = ({closeModal})=>{
     
     const [src,setSrc]= useState('')
     const [address, setAddress] = useState();
     const [copied,setCopied] = useState(false)
-    useEffect(()=>{
-        setAddress(reactLocalStorage.getObject('user').btc_wallet[0].address)
-        console.log('BTC',reactLocalStorage.getObject('user'))
-        QRcode.toDataURL(reactLocalStorage.getObject('user').btc_wallet[0].address).then((data)=>{
-            setSrc(data)
+    useEffect(async ()=>{
+        
+        const Base_url = process.env.REACT_APP_BACKEND_URL;
+        await axios({
+            method: "POST",
+            url: `${Base_url}/threshold/checkwalletaddress`,
+            headers:{
+                'Content-Type':'application/json',
+                'Authorization':reactLocalStorage.get('token')
+            },
+            data:JSON.stringify({address:reactLocalStorage.getObject('user').btc_wallet[0].address,wallet_type:"BTC"})
         })
+        .then((res)=>{
+            console.log('WalletCheck',res.data)
+            if(res.data.status){
+                
+                setAddress(reactLocalStorage.getObject('user').btc_wallet[0].address)
+                QRcode.toDataURL(reactLocalStorage.getObject('user').btc_wallet[0].address).then((data)=>{
+                    setSrc(data)
+                })
+            }
+            else{
+                
+                setAddress('Invalid Wallet Address')
+                QRcode.toDataURL("Invalid Wallet Address").then((data)=>{
+                    setSrc(data)
+                })
+            }
+           
+            
+            
+        })
+        .catch((err)=>{
+
+            console.log(err)
+           
+            
+        })
+       
     },[])
     return (
         <div className="modalBackground">
