2f97eafea85c155181f41fd5ffa0d276fbcddc7b
TEMILOLUWA <bigdevtemy@gmail.com>
2023-01-12 18:00:13 +0100

    ok

--- src/layout/WithdrawalLayout/index.js
+++ src/layout/WithdrawalLayout/index.js
@@ -502,15 +502,15 @@ const Index =()=>{
         toast.error("You can not transact more than N10,000 per transaction on this KYC LEVEL.","KYC Restriction");
         return false;
        }
-       if(kycprogress === 55 && value > 200000){
-        toast.error("Sorry,you can not transact more than N200,000 per transaction on this KYC LEVEL.");
-        return false;
-       }
+    //    if(kycprogress === 55 && value > 200000){
+    //     toast.error("Sorry,you can not transact more than N200,000 per transaction on this KYC LEVEL.");
+    //     return false;
+    //    }
 
-       if(kycprogress === 100 && value > 1000000){
-        toast.error("Sorry,you can not transact more than N1,000,000 per transaction on this KYC LEVEL.");
-        return false;
-       }
+    //    if(kycprogress === 100 && value > 1000000){
+    //     toast.error("Sorry,you can not transact more than N1,000,000 per transaction on this KYC LEVEL.");
+    //     return false;
+    //    }
 
     
         
